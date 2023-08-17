8f76ec9d3f2d0f9140d2e236c35af86f28173d00
Yousef Al-waer <elwaeryousef@gmail.com>
2023-08-17 18:39:40 +0300

    solve small bug in the main file

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from pkg.documents.templates.replacement import replace_template_parts
 from pkg.documents.templates.requirement_data import get_requirements_data
 from pkg.helm.charts_finder import find_chart_directories
 from pkg.cmd.command_line import parse_command_line_args
-from pkg.helm.Template import load_readme_template, write_docs
+from pkg.helm.Template import load_readme_template
+from pkg.helm.utils import write_file
 
 
 def process_single_chart(chart_directory, template_files, output_file, dry_run):
@@ -13,7 +14,7 @@ def process_single_chart(chart_directory, template_files, output_file, dry_run):
     # init chart data
     chart_data = get_chart_data(chart_directory)
     # init requirements data
-    requirements_data = get_requirements_data(chart_directory)
+    requirements_data = get_requirements_data(chart_directory, chart_data)
     # replace template parts
     readme_file = replace_template_parts(readme_template, chart_data, requirements_data, chart_directory)
     # if dry run active just print the readme file
@@ -21,7 +22,7 @@ def process_single_chart(chart_directory, template_files, output_file, dry_run):
         print(readme_file)
     # else write the readme file to the output file
     else:
-        write_docs(readme_file, './' + output_file)
+        write_file(readme_file, './' + output_file)
 
 
 def full_run():
@@ -41,14 +42,16 @@ def full_run():
         process_single_chart(chart_directory, template_file, output_file, dry_run)
 
 
-def main():
-    # full_run()
-
+def testing_chart():
     chart_directory = './example-charts/full-template'
     template_files = 'README.md.gotmpl'
     output_file = "README.md"
     dry_run = False
     process_single_chart(chart_directory, template_files, output_file, dry_run)
+
+def main():
+    # full_run()
+    testing_chart()
 
 
 if __name__ == "__main__":
