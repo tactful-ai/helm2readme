54fd2a648f7e6a9189f4fb95f287491deefb6846
Yousef Al-waer <elwaeryousef@gmail.com>
2023-08-19 21:34:59 +0300

    adding the publish step to pypl

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='doxy-helm',
+    version='1.0.0',
+    description='Doxy-Helm is a command-line utility designed to streamline the process of creating comprehensive documentation for Helm charts. Helm charts provide a convenient way to package and deploy Kubernetes applications, but documenting their configurations can be time-consuming. Doxy-Helm automates this task by extracting information from Helm charts values files and templates and generating Markdown documentation.',
+    author='Yousef Alwaer',
+    author_email='elwaeryousef@gmail.com',
+    url='https://github.com/tactful-ai/doxy-helm',
+    packages=find_packages(),
+    install_requires=[
+        'argparse',
+        'typing',
+        'markdown',
+        'pyyaml'
+    ],
+)
