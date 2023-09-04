17cda38fd6fed782c7d6cea8aaba9406f9fe01f2
Yousef Al-waer <elwaeryousef@gmail.com>
2023-09-04 16:11:24 +0300

    update the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='doxy-helm',
-    version='1.0.4',
+    version='1.0.5',
     package_dir={"": "."},
     description='Doxy-Helm is a command-line utility designed to streamline the process of creating comprehensive documentation for Helm charts. Helm charts provide a convenient way to package and deploy Kubernetes applications, but documenting their configurations can be time-consuming. Doxy-Helm automates this task by extracting information from Helm charts values files and templates and generating Markdown documentation.',
     author='Yousef Alwaer',
